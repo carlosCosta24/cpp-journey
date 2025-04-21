b0eba64f43b1969387fe8d0cd5d579af4d561ed3
carlosCosta <bodymgahed@yahoo.com>
2025-04-20 23:43:38 -0300

    print function exc

---
+++ print/pragraph.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "I love programming!" << std::endl
+              << std::endl;
+    std::cout << "I promise to be the best developer ever!" << std::endl
+              << std::endl;
+    std::cout << "I know it will take sone time to practice,but i " << std::endl;
+    std::cout << "will achieve my goal" << std::endl
+              << std::endl;
+    std::cout << "Best regards," << "carlos costa" << std::endl;
+}
---
+++ print/print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "**************************************" << std::endl;
+    std::cout << "Name : carlos costa " << std::endl;
+    std::cout << "Age : 26 " << std::endl;
+    std::cout << "City : aracaju " << std::endl;
+    std::cout << "Country : Brazil " << std::endl;
+    std::cout << "**************************************" << std::endl;
+
+    return 0;
+}
