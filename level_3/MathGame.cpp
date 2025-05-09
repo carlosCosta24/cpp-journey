0974da4dce3396d8c0f18b3145fb6580f34cacc9
carlosCosta <bodymgahed@yahoo.com>
2025-05-09 01:05:32 -0300

    Math game project

--- level_3/MathGame.cpp
+++ level_3/MathGame.cpp
@@ -12,120 +12,173 @@ enum enGameLevels
 };
 enum enOperation
 {
-    add = 1,
-    sub = 2,
-    mult = 3,
-    div = 4,
+    Add = 1,
+    Sub = 2,
+    Mult = 3,
+    Div = 4,
+    Mix = 5,
 };
-enum enStatus
-{
-    pass = 1,
-    fail = 2,
-};
-struct stGameInfo
-{
-    short NumberOfQuestions;
-    enGameLevels QuestionsLevel;
-    enOperation OperationType;
-    short RightAnswer;
-    short WrongAnswer;
-    enStatus status;
-    int Result;
-};
-
 int RandomNumber(int From, int To)
 {
     return rand() % (To - From + 1) + From;
 }
-
-int QuestionGenerator(int Level)
-{
-    int First, Second;
-    switch (Level)
-    {
-    case enGameLevels::easy:
-        First = RandomNumber(1, 10);
-        Second = RandomNumber(1, 10);
-        return (First, Second);
-    case enGameLevels::medium:
-        First = RandomNumber(10, 20);
-        Second = RandomNumber(10, 20);
-        return (First, Second);
-    case enGameLevels::hard:
-        First = RandomNumber(50, 80);
-        Second = RandomNumber(50, 80);
-        return (First, Second);
-    case enGameLevels::mix:
-        First = RandomNumber(1, 80);
-        Second = RandomNumber(1, 80);
-        return (First, Second);
-    default:
-        First = RandomNumber(1, 10);
-        Second = RandomNumber(1, 10);
-        return (First, Second);
-    }
-}
-
-void ResultPrinter(stGameInfo Info)
-{
-    cout << "_____________________" << endl;
-    cout << "Final Results is " << Info.status;
-    cout << "_____________________" << endl;
-    cout << "Number of questions: " << Info.NumberOfQuestions << endl;
-    cout << "Questions level: " << Info.QuestionsLevel << endl;
-    cout << "Operation type: " << Info.OperationType << endl;
-    cout << "Number of right answers: " << Info.RightAnswer << endl;
-    cout << "Number og wrong answers: " << Info.WrongAnswer << endl;
-    cout << "_________________________" << endl;
-}
-
-char OperationSign(enOperation Operation)
+string GetOperationSym(enOperation Operation)
 {
     switch (Operation)
     {
-    case enOperation::add:
-        return '+';
-
-    case enOperation::sub:
-        return '-';
-    case enOperation::mult:
-        return '*';
-    case enOperation::div:
-        return '/';
+    case enOperation::Add:
+        return "+";
+    case enOperation::Sub:
+        return "-";
+    case enOperation::Mult:
+        return "*";
+    case enOperation::Div:
+        return "/";
+    case enOperation::Mix:
+        return "+";
     default:
-        return '+';
+        return "+";
     }
 }
-
-void QuestionPrinter(int Part, int Total, int Level, enOperation Operation)
+string GetQuestionLevelString(enGameLevels Level)
 {
+    string Operations[4] = {"easy", "medium", "hard", "mix"};
 
-    int First, Second = QuestionGenerator(Level);
-    cout << "Question [" << Part << "/" << Total << "]" << endl;
-    cout << First << endl;
-    cout << Second << OperationSign(Operation);
+    return Operations[Level - 1];
 }
-
-int ResultCalculator(int First, int Second, char Operation)
+enOperation GetRandomOperation()
+{
+    return (enOperation)RandomNumber(1, 4);
+}
+void SetTextColor(bool Answer, string Message)
+{
+    if (Answer)
+        cout << "\e[32m" << Message << "! \e[0m" << endl;
+    else
+        cout << "\e[31m" << Message << "! \e[0m" << endl;
+}
+int ResultCalculator(int First, int Second, enOperation Operation)
 {
     int Result;
     switch (Operation)
     {
-    case '+':
+    case enOperation::Add:
         Result = First + Second;
         return Result;
-    case '-':
+    case enOperation::Sub:
         Result = First - Second;
         return Result;
-    case '*':
+    case enOperation::Mult:
         Result = First * Second;
         return Result;
-    case '/':
-        Result = First / Second;
+    case enOperation::Div:
+        (Second != 0) ? (Result = First / Second) : 0;
         return Result;
     default:
-        return 0;
+        return First + Second;
     }
+}
+struct stQuestion
+{
+    short FirstNumber = 0;
+    short SecondNumber = 0;
+    enOperation Operation;
+    enGameLevels Level;
+    short CorrectAnswer = 0;
+    short PlayAnswer = 0;
+    bool AnswerResult = false;
+};
+struct stGameInfo
+{
+    stQuestion QuestionList[100];
+    short NumberOfQuestions;
+    enGameLevels QuestionsLevel;
+    enOperation OperationType;
+    short RightAnswer = 0;
+    short WrongAnswer = 0;
+    bool Pass = false;
+};
+stQuestion QuestionGenerator(enGameLevels Level, enOperation Operation)
+{
+    stQuestion Question;
+
+    if (Level == enGameLevels::mix)
+        Level = (enGameLevels)RandomNumber(1, 3);
+
+    if (Operation == enOperation::Mix)
+        Operation = GetRandomOperation();
+
+    Question.Operation = Operation;
+
+    switch (Level)
+    {
+    case enGameLevels::easy:
+        Question.FirstNumber = RandomNumber(1, 10);
+        Question.SecondNumber = RandomNumber(1, 10);
+        break;
+    case enGameLevels::medium:
+        Question.FirstNumber = RandomNumber(10, 50);
+        Question.SecondNumber = RandomNumber(10, 50);
+        break;
+    case enGameLevels::hard:
+        Question.FirstNumber = RandomNumber(50, 100);
+        Question.SecondNumber = RandomNumber(50, 100);
+        break;
+    default:
+        Question.FirstNumber = RandomNumber(1, 10);
+        Question.SecondNumber = RandomNumber(1, 10);
+        break;
+    }
+    Question.CorrectAnswer = ResultCalculator(Question.FirstNumber, Question.SecondNumber, Question.Operation);
+    Question.Level = Level;
+
+    return Question;
+}
+void AskAndFeedBack(stGameInfo &Info)
+{
+
+    for (short NumberOfQuestion = 0; NumberOfQuestion < Info.NumberOfQuestions; NumberOfQuestion++)
+    {
+        cout << "Question [" << NumberOfQuestion + 1 << " / " << Info.NumberOfQuestions << "]" << endl;
+        cout << Info.QuestionList[NumberOfQuestion].FirstNumber << " "
+             << GetOperationSym(Info.QuestionList[NumberOfQuestion].Operation) << " "
+             << Info.QuestionList[NumberOfQuestion].SecondNumber << " = ";
+
+        cin >> Info.QuestionList[NumberOfQuestion].PlayAnswer;
+
+        if (Info.QuestionList[NumberOfQuestion].PlayAnswer == Info.QuestionList[NumberOfQuestion].CorrectAnswer)
+        {
+            SetTextColor(true, "Correct answer");
+            Info.RightAnswer++;
+        }
+        else
+        {
+            SetTextColor(false, "Wrong answer");
+            Info.WrongAnswer++;
+        }
+    }
+
+    Info.Pass = (Info.RightAnswer >= Info.WrongAnswer);
+}
+void Play(int NumberOfQuestions)
+{
+
+    stGameInfo Info;
+    Info.NumberOfQuestions = NumberOfQuestions;
+    Info.QuestionsLevel = enGameLevels::easy;
+    Info.OperationType = enOperation::Mix;
+
+    for (short Question = 0; Question < Info.NumberOfQuestions; Question++)
+    {
+        Info.QuestionList[Question] = QuestionGenerator(Info.QuestionsLevel, Info.OperationType);
+    }
+
+    AskAndFeedBack(Info);
+
+    cout << "Quiz completed!\nRight answers: " << Info.RightAnswer
+         << "\nWrong Answers: " << Info.WrongAnswer << endl;
+
+    Info.Pass ? SetTextColor(true, "You passed") : SetTextColor(false, "You didn't passed");
 }
 int ReadNumber(string Message)
 {
@@ -134,71 +187,11 @@ int ReadNumber(string Message)
     cin >> Number;
     return Number;
 }
-stGameInfo Play(int NumberOfQuestions)
-{
-
-    stGameInfo Info;
-    short Level, RightCount, WrongCount, Status;
-    char Operation;
-    int first, second = 0;
-
-    for (int Rounds = 1; Rounds <= NumberOfQuestions; Rounds++)
-    {
-        Info.QuestionsLevel = (enGameLevels)ReadNumber("Enter Questions Level [1] Easy, [2]Med, [3]Hard, [4]Mix? ");
-        cout << endl;
-        Info.OperationType = (enOperation)ReadNumber("Enter Operation Type [1] Add, [2]Sub, [3]Mul, [4]Div, [5]Mix? ");
-        cout << endl;
-        Info.RightAnswer = RightCount;
-        Info.WrongAnswer = WrongCount;
-        Info.status = (enStatus)Status;
-        Info.Result = ReadNumber(" ");
-        first, second = QuestionGenerator(Info.QuestionsLevel);
-        Operation = OperationSign(Info.OperationType);
-        Level = Info.QuestionsLevel;
-
-        if (ResultCalculator(first, second, Operation) == Info.Result)
-        {
-            RightCount++;
-            cout << "Right answer :-)" << endl;
-        }
-        else
-        {
-            WrongCount++;
-            cout << "The right answer is:" << ResultCalculator(first, second, Operation) << ":-)" << endl;
-        }
-
-        if (RightCount >= NumberOfQuestions / 2)
-        {
-            Status = enStatus::pass;
-        }
-        else
-        {
-            Status = enStatus::fail;
-        }
-        ResultPrinter(Info);
-    }
-    return Info;
-}
-
-void GameStarter()
-{
-    char PlayAgain = 'Y';
-    do
-    {
-        system("clear");
-        stGameInfo Info = Play(3);
-        cout << "Game over!";
-
-        cout << "Do you want to play again? Y/N: ";
-        cin >> PlayAgain;
-
-    } while (PlayAgain == 'Y' || PlayAgain == 'y');
-}
 
 int main()
 {
 
     srand((unsigned)time(NULL));
-    GameStarter();
+    Play(ReadNumber("Enter the Number of Questions: "));
     return 0;
 }
