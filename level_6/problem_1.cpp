ef5361d7dac73efc0f14ecd69e7b130d1595de15
carlosCosta <bodymgahed@yahoo.com>
2025-06-25 04:09:04 -0300

    number to text

--- level_6/problem_1.cpp
+++ level_6/problem_1.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
-#include <>
+#include <string>
 using namespace std;
 
-int ReciveInput(){
-string Input;
+int ReadInput(){
+int Input;
 cout << "Please enter a number?";
 cin >> Input;
-return stoi(Input);
+return Input;
 }
 
+string NumberToText(int Number){
+    if(Number == 0){
+    return "Zero";
+}if(Number >= 1 && Number <= 19){
+    string Teen [] = {"", "One","Two","Three","Four","Five","Six","Seven", "Eight",
+    "Nine","Ten","Eleven","Twelve","Thirteen","Fourteen", "Fifteen","Sixteen",
+    "Seventeen","Eighteen","Nineteen"};
+    return Teen[Number];
+}if(Number >= 20 && Number <= 99){
+    string Arr [] = {"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety" };
+    return Arr[Number / 10] + " " + NumberToText(Number % 10) ;
 
+}if(Number >= 100 && Number <= 9999) {
+    return " Hundred " + NumberToText(Number % 100);
+}
+    return "";
+};
+
+
+
+
+int main(){
+cout << NumberToText(ReadInput());
+return 0;
+};
