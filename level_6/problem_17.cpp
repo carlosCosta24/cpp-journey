e7904946ab2abd3399475b52557551ab8dc8e9c6
carlosCosta <bodymgahed@yahoo.com>
2025-07-13 04:44:43 -0300

    Number of days between two dates

---
+++ level_6/problem_17.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+struct stDate {
+    short Year;
+    short Month;
+    short Day;
+};
+bool YearChecker(short Input) {
+
+    return (Input % 4 == 0 && Input % 100 != 0) || (Input % 400 == 0);
+}
+bool IsLongMonth(short month) {
+
+    return month == 1 || month == 3 || month == 5 ||
+           month == 7 || month == 8 || month == 10 || month == 12;
+}
+short NumberOfDays(short Year , short Month) {
+
+    if (Month < 1 || Month > 12) return 0;
+    return(Month == 2)
+    ?(YearChecker(Year)?  29 : 28)
+    : ((IsLongMonth(Month))? 31: 30);
+}
+bool IsLastDay(short Year, short Month, short Day) {
+    short NumberOfDaysInMonth = NumberOfDays(Year, Month);
+    return (NumberOfDaysInMonth == Day) ? true : false;
+}
+bool IsLastMonth(short Month) {
+    return (Month == 12 ) ? true : false;
+}
+stDate ReadDate() {
+    stDate Date;
+    cout << "Please enter a day: ";
+    cin >> Date.Day;
+    cout << "Please enter a month: ";
+    cin >> Date.Month;
+    cout << "Please enter a year: ";
+    cin >> Date.Year;
+    return Date;
+}
+stDate IncreaseDayByOne(stDate Date) {
+    if (IsLastDay(Date.Year, Date.Month, Date.Day) && IsLastMonth(Date.Month)) {
+        Date.Year ++;
+        Date.Month = 1;
+        Date.Day = 1;
+    }else {
+        short TotalDaysInMonth = NumberOfDays(Date.Year, Date.Month);
+        if (Date.Day < TotalDaysInMonth) {
+            Date.Day ++;
+        }else {
+            Date.Month ++;
+            Date.Day = 1;
+        }
+    }
+    return Date;
+}
+bool IsDate1BeforeDate2(stDate First, stDate Second) {
+    return  (First.Year < Second.Year) ? true :
+    ((First.Year == Second.Year) ? (First.Month < Second.Month ? true :
+    (First.Month == Second.Month ? First.Day < Second.Day : false)) : false);
+}
+bool DateIsEqual(stDate First, stDate Second) {
+    return (First.Year == Second.Year) ?
+    ((First.Month == Second.Month) ? ((First.Day == Second.Day) ? true : false) : false) : false;
+}
+int DiffCalculate(stDate Date1, stDate Date2, bool IncludeLastDay = false) {
+    int DiffDays = 0;
+    while (IsDate1BeforeDate2(Date1, Date2)) {
+        DiffDays ++;
+        Date1 = IncreaseDayByOne(Date1);
+    }
+    return IncludeLastDay ? ++DiffDays : DiffDays;
+}
+
+int main() {
+    stDate Date1 =ReadDate();
+    stDate Date2 =ReadDate();
+    cout << "Difference is: "<< DiffCalculate(Date1, Date2) << " Days(s)" <<endl;
+    cout << "Difference (including end Day) is: " << DiffCalculate(Date1, Date2, true)  << endl;
+    return 0;
+
+
+
+}
