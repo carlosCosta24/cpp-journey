5a1b1106c82572fe5b6bebd254c660f6e61066b6
carlosCosta <bodymgahed@yahoo.com>
2025-08-27 06:01:05 -0300

    constructor & copy constructor

---
+++ level_8/copy_constructors.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+class clsAddress {
+private:
+    string _AddressLine1;
+    string _AddressLine2;
+    string _POBox;
+    string _ZipCode;
+public:
+    // constructor
+    // it's prohibited to initiate an empty class
+    clsAddress(string AddressLine1, string AddressLine2, string POBox, string ZipCode) {
+        _AddressLine1 = AddressLine1;
+        _AddressLine2 = AddressLine2;
+        _POBox = POBox;
+        _ZipCode = ZipCode;
+    };
+
+    //copy constructor
+
+    clsAddress(clsAddress & Prev) {
+        _AddressLine1 = Prev.AddressLine1();
+        _AddressLine2 = Prev.AddressLine2();
+        _POBox = Prev.POBox();
+        _ZipCode = Prev.ZipCode();
+
+    }
+
+    void SetAddressLine1(string AddressLine1) {
+        _AddressLine1 = AddressLine1;
+    }
+
+    string AddressLine1() {
+        return _AddressLine1;
+    }
+
+    void SetAddressLine2(string AddressLine2) {
+        _AddressLine2 = AddressLine2;
+    }
+
+    string AddressLine2() {
+        return _AddressLine2;
+    }
+    void SetPOBox(string POBox) {
+        _POBox = POBox;
+    }
+    string POBox() {
+        return _POBox;
+    }
+    void SetZipCode(string ZipCode) {
+        _ZipCode = ZipCode;
+    }
+    string ZipCode() {
+        return _ZipCode;
+    }
+    void Print() {
+        cout << "\nAddress Details:\n";
+        cout << "------------------------";
+        cout << "\nAddressLine1: " << _AddressLine1 <<endl;
+        cout << "AddressLine2: " << _AddressLine2 << endl;
+        cout << "POBox       : " << _POBox << endl;
+        cout << "ZipCode     : " << _ZipCode << endl;
+    }
+};
+
+int main() {
+    clsAddress Address1("Milicio machado","B 1427 ","11192","5555");
+    Address1.Print();
+    return 0;
+}
