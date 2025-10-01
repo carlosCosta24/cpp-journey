f9c4b480c1551f117cf33a2482131fffbb7b9ee0
carlosCosta <bodymgahed@yahoo.com>
2025-10-01 09:53:54 -0300

    restructure , add login fun, main menu operation

--- level_6/Atm.cpp
+++ level_6/Atm.cpp
@@ -19,25 +19,9 @@ enum enATMMainMenu {
     enLogout = 5,
 };
 
-const string ClientsList = "bank.txt";
-
-void MainMenuScreen () {
-    cout << "=======================================================" << endl;
-    cout << "\t\t ATM Main Menu Screen "<< endl;
-    cout << "=======================================================" << endl;
-    cout << "[1] Quick Withdraw "<<endl;
-    cout << "[2] Normal Withdraw "<<endl;
-    cout << "[3] Deposit "<<endl;
-    cout << "[4] Check Balance "<<endl;
-    cout << "[5] Logout "<<endl;
-    cout << "=======================================================" << endl;
-}
-
-void LoginScreen () {
-    cout << "--------------------------------------------"<< endl;
-    cout << "\t\t\t ATM Login Screen "<< endl;
-    cout << "--------------------------------------------"<< endl;
-}
+const string Database = "bank.txt";
+stClient CurrentClient;
+void LogIn();
 
 vector<string> vSpliter(string &s, string delim) {
     vector<string> vResult;
@@ -89,8 +73,8 @@ vector<stClient> LoadClients(string FileName) {
 }
 
 bool CheckClientAndPassword(string FileName, string Account,string Password ) {
-    vector <stClient> vClientsList = LoadClients(FileName);
-    for (stClient Client : vClientsList) {
+    vector <stClient> vDatabase = LoadClients(FileName);
+    for (stClient Client : vDatabase) {
         if (Client.account == Account) {
             if (Client.password == Password) {
                 return true;
@@ -101,20 +85,19 @@ bool CheckClientAndPassword(string FileName, string Account,string Password ) {
 
 }
 
-void QuickWithdrawMenu() {
-    cout << "========================================================" << endl;
-    cout << "\t\t Quick Withdraw "<< endl;
-    cout << "==========================================================" << endl;
-    cout << "\t\t[1] 20";
-    cout << "\t\t\t\t[2] 50"<<endl;
-    cout << "\t\t[3] 100";
-    cout << "\t\t\t\t[4] 200"<<endl;
-    cout << "\t\t[5] 400";
-    cout << "\t\t\t\t[6] 600"<<endl;
-    cout << "\t\t[7] 800";
-    cout << "\t\t\t\t[8] 1000"<<endl;
-    cout << "\t\t[9] Exit"<<endl;
-    cout << "==========================================================" << endl;
+bool LoadClient(string Username, string Password) {
+    if (CheckClientAndPassword(Database, Username, Password)) {
+        return true;
+    }else {
+        return false;
+    }
+
+}
+
+void LoginScreen () {
+    cout << "--------------------------------------------"<< endl;
+    cout << "\t\t\t ATM Login Screen "<< endl;
+    cout << "--------------------------------------------"<< endl;
 }
 
 int ReadMenuOptions() {
@@ -125,35 +108,168 @@ int ReadMenuOptions() {
     return Option;
 }
 
+void QuickWithdrawMenu() {
+    cout << "========================================================" << endl;
+    cout << "\t\t Quick Withdraw "<< endl;
+    cout << "========================================================" << endl;
+    cout << "\t\t[1] 20";
+    cout << "\t\t\t\t[2] 50"<<endl;
+    cout << "\t\t[3] 100";
+    cout << "\t\t\t\t[4] 200"<<endl;
+    cout << "\t\t[5] 400";
+    cout << "\t\t\t\t[6] 600"<<endl;
+    cout << "\t\t[7] 800";
+    cout << "\t\t\t\t[8] 1000"<<endl;
+    cout << "\t\t[9] Exit"<<endl;
+    cout << "==========================================================" << endl;
+    ReadMenuOptions();
+}
+
+void NormalWithdrawMenu() {
+    cout << "========================================================" << endl;
+    cout << "\t\t Normal Withdraw "<< endl;
+    cout << "========================================================" << endl;
+}
+
+void Deposit() {
+    double DepositAmount;
+    cout << "Enter Deposit Amount: "<< endl;
+    cin >> DepositAmount;
+    CurrentClient.balance += DepositAmount;
+
+}
+
+double ReadAmount() {
+    double Amount;
+    cout << "Please Enter Amount : ";
+    cin >> Amount;
+
+    return Amount;
+}
+
 void ClearScreen() {
     std::cout << "\033[2J\033[1;1H";
 }
 
-void CheckBalance(string Account) {
-    vector <stClient> vClientsList = LoadClients(ClientsList);
-    for (stClient& Client : vClientsList) {
-        if (Client.account == Account) {
-            cout << "Your Balance is " << Client.balance << endl;
-            return;
+stClient LoadClientInfo(string& ClientName) {
+    vector<stClient> vClients = LoadClients(Database);
+    stClient TargetClient;
+    for (stClient& Client : vClients) {
+        if (Client.account == ClientName) {
+              TargetClient = Client;
         }
     }
-    cout << "Account Not Found" << endl;
+    return TargetClient;
 }
-// void PerformMainMenu(enATMMainMenu Option) {
-//     ReadMenuOptions();
-//     switch (Option) {
-//         case enQuickWithDraw: {
-//             ClearScreen();
-//             QuickWithdrawMenu();
-//             break;
-//         }
-//         case enNormalWithDraw: {
-//             ClearScreen();
-//             NormalWithdraw();
-//         }
-//     }
-// }
+void CheckBalance() {
+    stClient Client = LoadClientInfo(CurrentClient.account);
+    cout << "Your Balance is: " << CurrentClient.balance << endl;
+}
+
+char Capitalize(const char & Character) {
+    return toupper(Character);
+}
+
+void NormalWithdraw() {
+    stClient Client;
+    double WithdrawAmount;
+    do {
+        cout << "Enter an amount multiple of 5's ? "<< endl;
+        WithdrawAmount = ReadAmount();
+    } while (WithdrawAmount / 5.0 != 0 );
+
+    if (Client.balance >= WithdrawAmount) {
+        cout << "the amount exceeds your balance, make another choice, "
+                "your current balance is " << Client.balance << endl;
+        return;
+    }
+    char Answer;
+    cout << "Are you sure you want to perform this transaction? Y/N? "<< endl;
+    cin >> Answer;
+    Answer = Capitalize(Answer);
+
+    if (Answer == 'Y') {
+        Client.balance -= WithdrawAmount;
+        cout << "Done successfully, Your current balance is " << Client.balance << endl;
+    }
+}
+
+void PerformMainMenu(enATMMainMenu Option) {
+    switch (Option) {
+        case enQuickWithDraw: {
+            ClearScreen();
+            QuickWithdrawMenu();
+            break;
+        }
+        case enNormalWithDraw: {
+            ClearScreen();
+            NormalWithdraw();
+            break;
+        }
+        case enDeposit: {
+            ClearScreen();
+            Deposit();
+            break;
+        }
+        case enCheckBalance: {
+            ClearScreen();
+            CheckBalance();
+            break;
+
+        }
+        case enLogout: {
+            ClearScreen();
+            LoginScreen();
+            LogIn();
+            break;
+        }
+        default: {
+            ClearScreen();
+            cout << "Please enter a valid option!" << endl;
+            break;
+        }
+    }
+}
+
+void MainMenuScreen () {
+    ClearScreen();
+    cout << "=======================================================" << endl;
+    cout << "\t\t ATM Main Menu Screen "<< endl;
+    cout << "=======================================================" << endl;
+    cout << "[1] Quick Withdraw "<<endl;
+    cout << "[2] Normal Withdraw "<<endl;
+    cout << "[3] Deposit "<<endl;
+    cout << "[4] Check Balance "<<endl;
+    cout << "[5] Logout "<<endl;
+    cout << "=======================================================" << endl;
+    PerformMainMenu((enATMMainMenu) ReadMenuOptions());
+}
+
+void LogIn() {
+    string ClientName , Password;
+    bool LogInFailed = false;
+    do {
+    LoginScreen();
+
+    if (LogInFailed) {
+        cout << "Username / password is not correct!" << endl;
+
+    }
+        cout << "Please enter your username: "<< endl;
+
+        cin >> ClientName;
+
+        cout << "Please enter your password: "<<endl;
+
+        cin >> Password;
+
+        LogInFailed = !LoadClient( ClientName, Password);
+    }while (LogInFailed);
+
+    MainMenuScreen();
+
+}
 
 int main() {
-
+    LogIn();
 }
