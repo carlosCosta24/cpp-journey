4cfb5f6c66fd278139300ccd100a1e82bbcce8f6
carlosCosta <bodymgahed@yahoo.com>
2025-10-21 09:04:44 -0300

    restructure the main.cpp across the branches

--- level_9/update_client_opp/main.cpp
+++ level_9/update_client_opp/main.cpp
@@ -2,7 +2,64 @@
 #include "clsBankClient.h"
 #include "clsInputValidate.h"
 #include <iomanip>
-using namespace std;
+
+void ReadClientInfo(clsBankClient& Client) {
+
+    cout << "\nEnter Client FirstName:";
+    Client.SetFirstName(clsInputValidate::ReadString());
+
+    cout << "\nEnter Client LastName:";
+    Client.SetLastName(clsInputValidate::ReadString());
+
+    cout << "\nEnter Client Email:";
+    Client.SetEmail(clsInputValidate::ReadString());
+
+    cout << "\nEnter Client PhoneNumber:";
+    Client.SetPhone(clsInputValidate::ReadString());
+
+    cout << "\nEnter Client Password:";
+    Client.SetPassword(clsInputValidate::ReadString());
+
+    cout << "\nEnter Client Balance:";
+    Client.setBalance(clsInputValidate::ReadFloatNumber());
+
+}
+
+void UpdateClient() {
+    string AccountNumber = "";
+
+    cout << "\nPlease enter client account number:";
+    AccountNumber = clsInputValidate::ReadString();
+    while (!clsBankClient::IsClientExist(AccountNumber)) {
+        cout << "\nPlease enter account number again: ";
+        AccountNumber = clsInputValidate::ReadString();
+    }
+    clsBankClient Client = clsBankClient::Find(AccountNumber);
+    Client.Print();
+
+    cout << "\n\nUpdate Client Info";
+    cout << "\n---------------------\n";
+
+    ReadClientInfo(Client);
+
+    clsBankClient::enSaveResult SaveResult = Client.Save();
+
+    switch (SaveResult) {
+        case clsBankClient::enSaveResult::svSaved: {
+            cout << "\n\nUpdated Successfully ";
+            Client.Print();
+            break;
+        }
+        case clsBankClient::enSaveResult::svFailed: {
+            cout << "\n\nUpdated Failed ";
+            break;
+        }
+    }
+
+
+
+}
+
 void PrintClientRecord(clsBankClient Client) {
     cout << "| " << left << setw(15) << Client.AccountNumber();
     cout << "| " << left << setw(20) << Client.FullName();
@@ -11,6 +68,7 @@ void PrintClientRecord(clsBankClient Client) {
     cout << "| " << left << setw(10) << Client.GetPassword();
     cout << "| " << left << setw(12) << Client.GetBalance();
 }
+
 void ShowClientsList() {
     vector <clsBankClient> vClients = clsBankClient::GetClientsList();
     cout << "\n\t\t\t\tClients List(" << vClients.size() << ") Client/s" << endl;
@@ -33,7 +91,8 @@ void ShowClientsList() {
 
 
 }
+
 int main() {
-    ShowClientsList();
+    UpdateClient();
     return 0;
 }
