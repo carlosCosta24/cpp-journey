38a6377d28a519f3adbd10f14514a1acabdd03be
carlosCosta <bodymgahed@yahoo.com>
2025-10-31 14:17:58 -0300

    Adding locking system fun

--- level_9/ui_opp/clsLogScreen.h
+++ level_9/ui_opp/clsLogScreen.h
@@ -9,12 +9,21 @@ using namespace std;
 
 class clsLoginScreen : protected clsScreen {
     private:
-    static void _Login() {
+    static bool _Login() {
         bool LoginFailed = false;
         string UserName, Password;
+        short Trails = 3;
         do {
+            if (LoginFailed && Trails <= 1) {
+                cout << "Your have been locked out of the system \n"
+                        "due to too many login attempts,\n"
+                        "contact the bank" << endl;
+                return false;
+            }
             if (LoginFailed ) {
+                Trails -= 1;
                 cout << "\nIncorrect UserName / Password! " << endl;
+                cout << "\nYour Have (" << Trails << ") Trail(s) left" << endl;
             }
             cout << "Enter Username: ";
             cin >> UserName;
@@ -26,11 +35,12 @@ class clsLoginScreen : protected clsScreen {
             LoginFailed = CurrentUser.IsEmpty();
         }while(LoginFailed);
         clsMainScreen::ShowMainMenu();
+        return true;
     }
     public:
-    static void LoginScreen() {
+    static bool LoginScreen() {
         _ScreenHeader("\t  Login Screen");
-        _Login();
+        return _Login();
     }
 
 };
--- level_9/ui_opp/ui_main.cpp
+++ level_9/ui_opp/ui_main.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int main() {
     char Exit = 'n';
     while (Exit == 'n') {
-        clsLoginScreen::LoginScreen();
+        if (clsLoginScreen::LoginScreen() == false ) {
+            return 0;
+        };
         cout << "Do you Want to Exit? y/n: " << endl;
         cin >> Exit;
         clsUtil::Capitalize(Exit);
