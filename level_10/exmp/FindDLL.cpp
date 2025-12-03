72fca81d73fb54b23f1bf1a4659349cb9b9ea27c
carlosCosta <bodymgahed@yahoo.com>
2025-12-03 11:44:09 -0300

    DublyLinkedList Find

---
+++ level_10/exmp/FindDLL.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int Data;
+    Node* Next;
+    Node * Prev;
+};
+
+void PrintLinkedList(Node * Head) {
+    while (Head != NULL) {
+
+        cout << Head->Data << endl;
+        Head = Head->Next;
+    }
+}
+void Find(Node * Head, int Target ) {
+
+    if (Head == NULL) {
+        cout << "Linked List is Empty"<<endl;
+        return;
+    }
+    while (Head != NULL && Head->Data != Target) {
+        if (Head->Next == NULL) {
+            cout << "Target have not been found!!!" << endl;
+        }
+        cout << "searching...."<<endl;
+        Head = Head->Next;
+    }
+    cout << "Target found: " << endl;
+    cout << Head->Data;
+
+
+}
+
+void InsertAtBeginningDLL(Node* &head, int data) {
+    // creat a new node
+    // assign value to the node
+    // Set the next pointer of the new node to the current head of the list.
+    // Set the previous pointer of the current head to the new node.
+    // Set the new node as the new head of the list.
+    Node * New = new Node();
+    New->Data = data;
+    New->Next = head;
+    New->Prev = NULL;
+
+    if (head != NULL) {
+        head->Prev = New;
+    }
+    head = New;
+}
+
+int main() {
+    Node * Head = NULL;
+    InsertAtBeginningDLL(Head, 100);
+    InsertAtBeginningDLL(Head, 90);
+    InsertAtBeginningDLL(Head, 80);
+    InsertAtBeginningDLL(Head, 70);
+    InsertAtBeginningDLL(Head, 60);
+    InsertAtBeginningDLL(Head, 50);
+
+    PrintLinkedList(Head);
+
+    Find(Head, 10);
+    return 0;
+}
