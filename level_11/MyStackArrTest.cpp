691deac9aa9bdccd8976d03ac5916f8391711527
carlosCosta <bodymgahed@yahoo.com>
2026-01-09 23:50:37 -0300

    updating MyStack to use DynamicArr

---
+++ level_11/MyStackArrTest.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "clsMyStackArr.h"
+using namespace std;
+
+int main() {
+    clsMyStackArr <int> myStack;
+
+    myStack.Push(1);
+    myStack.Push(2);
+    myStack.Push(3);
+    myStack.Push(4);
+    cout << endl;
+    cout << "Stack Items: "<< endl;
+    myStack.Print();
+    cout << endl;
+
+    cout << "Pop the first item: " <<endl;
+    myStack.Pop();
+    myStack.Print();
+    cout << endl;
+
+    cout << "Stack Front: " << myStack.Top() << endl;
+    cout << "Stack Back: " << myStack.Bottom() << endl;
+    cout << "Stack Size: "<< myStack.Size() << endl;
+
+    cout << "Item on the 2 Index: ";
+    cout << myStack.GetItem(1)  << endl;
+
+    cout << "Reversing the Stack: ";
+    myStack.Reverse();
+    myStack.Print();
+    cout << endl;
+
+    cout << "Updating the first item: ";
+    myStack.UpdateItem(0,500);
+    myStack.Print();
+    cout << endl;
+
+    cout << "Inserting after the first item: ";
+    myStack.InsertAfter(1,700);
+    myStack.Print();
+    cout << endl;
+
+    cout << "Inserting at the beginning: ";
+    myStack.InsertAtFront(600);
+    myStack.Print();
+    cout << endl;
+
+    cout << "Inserting at the end: ";
+    myStack.InsertAtBack(400);
+    myStack.Print();
+    cout << endl;
+
+    cout << "Clearing the Stack : ";
+    myStack.Clear();
+    myStack.Print();
+    return 0;
+}
---
+++ level_11/clsMyStackArr.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include "clsMyQueueArr.h"
+using namespace std;
+
+template<class T>
+class clsMyStackArr : public clsMyQueueArr<T>
+{
+public:
+    void Push(T Item) {
+        clsMyQueueArr<T>::_MyList.InsertArBegin(Item);
+    };
+    T Top(){
+        return clsMyQueueArr<T>::Front();
+    };
+    T Bottom(){
+        return clsMyQueueArr<T>::Back();
+    };
+};
