1a18fac7dfdccd005ef88c7e5bf17f615ac0c163
carlosCosta <bodymgahed@yahoo.com>
2026-01-14 02:50:05 -0300

    queue system starter

---
+++ level_11/clsQueueLine.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <queue>
+#include <iostream>
+using namespace std;
+
+class clsQueueLine {
+    //date and time class
+    class clsDate {
+        private:
+
+    };
+    //IssueTicket Method
+    //PrintTicketsLineRTL
+    //PrintTicketsLineLTR
+    //PrintAllTickets
+    //ServeNextClient
+
+};
