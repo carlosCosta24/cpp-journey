33a0bb2661a2933a0234749c36d61f44e25b5635
carlos_costa <carloscosta.pen@gmail.com>
2026-01-19 03:42:36 -0300

    console app

---
+++ Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstConsoleProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to calculate the factorial: ");
+            int n = int.Parse(Console.ReadLine());
+            int fact = 1;
+
+            for (int i = 2; i <= n; i++) { 
+                fact = fact * i;
+            }
+            Console.WriteLine($"result of {n}! is: ");
+            Console.WriteLine(fact);
+        }
+    }
+}
