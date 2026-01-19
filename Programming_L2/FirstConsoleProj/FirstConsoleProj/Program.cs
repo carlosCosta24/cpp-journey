64a4ae609f890bddfede41aa6114ecc3ea269688
carlos_costa <carloscosta.pen@gmail.com>
2026-01-19 12:03:04 -0300

    scape charaters

--- Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
+++ Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace FirstConsoleProj
 {
@@ -6,15 +7,43 @@ namespace FirstConsoleProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number to calculate the factorial: ");
-            int n = int.Parse(Console.ReadLine());
-            int fact = 1;
+            Console.WriteLine("this write line fun");
+            Console.Write("this is a line without any space ");
+            Console.Write("and will be printed togther");
+            //formated string
+            Console.WriteLine(" ");
+            Console.WriteLine("{0} {1}", "Carols", "Costa");
 
-            for (int i = 2; i <= n; i++) { 
-                fact = fact * i;
-            }
-            Console.WriteLine($"result of {n}! is: ");
-            Console.WriteLine(fact);
+            Console.WriteLine("Useful Escape Characters: \n");
+
+            Console.WriteLine("NewLine:");
+            Console.WriteLine("Welcome to \n newline");
+
+            //tab
+            Console.WriteLine("Tab");
+            Console.WriteLine("Welcome to \tthe tap");
+
+            //Backspace
+            Console.WriteLine("Backspace");
+            Console.WriteLine("Welcome to \b this is the backspace");
+
+            //singel quote
+            Console.WriteLine("Single quote");
+            Console.WriteLine("Welcome to the \' my space \' ");
+
+            //Double quote
+            Console.WriteLine("Single quote");
+            Console.WriteLine("Welcome to the \" my space \" ");
+
+            //Backslash
+            Console.WriteLine("Backslash");
+            Console.WriteLine("Welcome this is a \\ backslash");
+
+            //alert
+            Console.WriteLine("\a");
+
+            Console.ReadKey();
+
         }
     }
 }
