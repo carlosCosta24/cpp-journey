6f1a8084ec56562ff81bf5474bde35bb05f9f63b
carlos_costa <carloscosta.pen@gmail.com>
2026-01-20 01:19:25 -0300

    Nullable

--- Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
+++ Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
@@ -6,41 +6,12 @@ namespace FirstConsoleProj
     internal class Program
 
     {
-        enum enWeekDays
-        { 
-            Monday,
-            Tuesday, 
-            Wednesday, 
-            Friday,
-            Saturday,
-            Sunday,
-
-        }
-        //when setting an value, every member after will be auto number
-        enum enCateg{
-            food,//0
-            art,//1
-            fashion = 3 // 3,
-            electronics//4,
-        }
-        //enums with numarical
-
-        enum enCategoriesNumerical : byte 
-        {
-            Electronics = 1,
-            Food = 2,
-            Art = 3,
-            Fashion = 4,
-
-        }
-
         static void Main(string[] args)
         {
 
-
-            enCateg myCat;
-            enCategoriesNumerical myCategoriesNumerical;
-            enWeekDays enWeekDays
+            //Nullable var
+            Nullable <int> i = null;
+            Console.WriteLine(i);
 
             Console.ReadKey();
 
