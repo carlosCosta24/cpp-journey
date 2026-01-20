d18ccf97f641adde2240eb5847ebe6131d467f2d
carlos_costa <carloscosta.pen@gmail.com>
2026-01-20 14:06:17 -0300

    Time span

--- Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
+++ Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
@@ -14,13 +14,45 @@ namespace FirstConsoleProj
 
             DateTime dt2 = new DateTime(2025, 12, 30, 5, 10, 26);
 
-            DateTime dt3 = DateTime.Now;
+            DateTime dt3 = new DateTime();
+
+            // ticks
+            Console.WriteLine(DateTime.MinValue.Ticks);
+            Console.WriteLine(DateTime.MaxValue.Ticks);
 
 
             Console.WriteLine(dt);       
             Console.WriteLine(dt1);
             Console.WriteLine(dt2);
-            Console.WriteLine(dt3);
+
+            DateTime Current = DateTime.Now;
+            DateTime Today = DateTime.Today;
+            DateTime CurrentZone = DateTime.UtcNow;
+            DateTime Max = DateTime.MaxValue;
+            DateTime Min = DateTime.MinValue;
+
+            Console.WriteLine("currentDateTime: " + Current);
+            Console.WriteLine("Today: " + Today);
+            Console.WriteLine("currentDateTimeUTC: " + CurrentZone);
+            Console.WriteLine("minDateTimeValue: " + Min);
+            Console.WriteLine("maxDateTimeValue: " + Max);
+
+            Console.WriteLine("\nTime Spane:");
+            DateTime Date = new DateTime(2026, 1, 1);
+
+            TimeSpan ts = new TimeSpan(24,24,24);
+            Console.WriteLine(ts);
+            Console.WriteLine(ts.Days);
+            Console.WriteLine(ts.Hours);
+            Console.WriteLine(ts.Minutes);
+            Console.WriteLine(ts.Seconds);
+            //Adding the timw spane to the date
+            DateTime newDate = Date.Add(ts);
+            Console.WriteLine(newDate);
+
+
+
+
 
 
             Console.ReadKey();
