38089192f1496ea032e00c3fc0ef66a5b2c4b34b
carlos_costa <carloscosta.pen@gmail.com>
2026-01-20 01:15:40 -0300

    enums

--- Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
+++ Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
@@ -4,32 +4,43 @@ using System.Dynamic;
 namespace FirstConsoleProj
 {
     internal class Program
+
     {
+        enum enWeekDays
+        { 
+            Monday,
+            Tuesday, 
+            Wednesday, 
+            Friday,
+            Saturday,
+            Sunday,
+
+        }
+        //when setting an value, every member after will be auto number
+        enum enCateg{
+            food,//0
+            art,//1
+            fashion = 3 // 3,
+            electronics//4,
+        }
+        //enums with numarical
+
+        enum enCategoriesNumerical : byte 
+        {
+            Electronics = 1,
+            Food = 2,
+            Art = 3,
+            Fashion = 4,
+
+        }
+
         static void Main(string[] args)
         {
-            //get the default value using default(type)
-            int i = default(int);
-       
-            float f = default(float);
-           
-            decimal d = default(decimal);
-           
-            bool b = default(b);
-           
-            char c = default(char);
-           
 
-            // c# 7.1 onward 
-            int i = default;
-           
-            float f = default;
-           
-            decimal d = default;
-           
-            bool b = default;
-          
-            char c = default;
-           
+
+            enCateg myCat;
+            enCategoriesNumerical myCategoriesNumerical;
+            enWeekDays enWeekDays
 
             Console.ReadKey();
 
