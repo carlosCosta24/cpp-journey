d7673084c9d3c7e55c484db2416824fb04af1a09
carlos_costa <carloscosta.pen@gmail.com>
2026-01-20 13:06:10 -0300

    anonymous typs

--- Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
+++ Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
@@ -9,9 +9,23 @@ namespace FirstConsoleProj
         static void Main(string[] args)
         {
 
-            //Nullable var
-            Nullable <int> i = null;
-            Console.WriteLine(i);
+            // anonymous types, are read only, can include another anonymous type
+            var student = new { Id = 1, 
+                FirstName = "carlos", 
+                LastName = "costa" ,
+                Address = new {
+                    City = "Aracaju",
+                    Country = "Brazil",
+                }
+            };
+            Console.WriteLine("\nExample: ");
+            Console.WriteLine($"Id: {student.Id}");
+            Console.WriteLine($"First Name: {student.FirstName}");
+            Console.WriteLine($"LastName: {student.LastName}");
+            Console.WriteLine($"city: {student.Address.City}");
+            Console.WriteLine($"Country: {student.Address.Country}");
+            Console.WriteLine(student);
+
 
             Console.ReadKey();
 
