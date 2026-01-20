3da2098f5fdeba374d9e5dc96027355f25928f9a
carlos_costa <carloscosta.pen@gmail.com>
2026-01-20 13:20:17 -0300

    structures

--- Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
+++ Programming_L2/FirstConsoleProj/FirstConsoleProj/Program.cs
@@ -3,29 +3,31 @@ using System.Dynamic;
 
 namespace FirstConsoleProj
 {
-    internal class Program
+    internal class Program { 
+        //structures
 
-    {
+        struct stStudent
+        {
+            public string FirstName;
+            public string LastName;
+        }
+
         static void Main(string[] args)
         {
 
-            // anonymous types, are read only, can include another anonymous type
-            var student = new { Id = 1, 
-                FirstName = "carlos", 
-                LastName = "costa" ,
-                Address = new {
-                    City = "Aracaju",
-                    Country = "Brazil",
-                }
-            };
-            Console.WriteLine("\nExample: ");
-            Console.WriteLine($"Id: {student.Id}");
-            Console.WriteLine($"First Name: {student.FirstName}");
-            Console.WriteLine($"LastName: {student.LastName}");
-            Console.WriteLine($"city: {student.Address.City}");
-            Console.WriteLine($"Country: {student.Address.Country}");
-            Console.WriteLine(student);
+        // Creating struct
+        stStudent Student;
 
+        Student.FirstName = "Carlos";
+        Student.LastName = "Costa";
+ 
+            Console.WriteLine("\nExample: ");
+
+            Console.WriteLine(Student.FirstName);
+            Console.WriteLine(Student.LastName);
+
+
+     
 
             Console.ReadKey();
 
